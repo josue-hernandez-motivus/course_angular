a31778cb820becc9037ad184a441dce781931b7e
Josué David Hernández Ramírez <josue.hernandezr@motivus.com>
2025-09-22 09:38:29 -0600

    feat: install and review basic types for TS 🚀

---
+++ typescript-intro/src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+// import { basicTypes } from './topics/basic-types';
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>Hello World</h1>
+`;
