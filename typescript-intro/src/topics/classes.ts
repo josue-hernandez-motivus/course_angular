64fcfe323000f5a70d71ec11acfee79cbb9cc38c
Josué David Hernández Ramírez <josue.hernandezr@motivus.com>
2025-09-22 17:28:15 -0600

    feat: preparing depedences injection 📚

--- typescript-intro/src/topics/classes.ts
+++ typescript-intro/src/topics/classes.ts
@@ -1,5 +1,6 @@
 export class Person {
-    public name: string;
+    public firstName: string;
+    public lastName: string;
     private address: string;
 
     // Usar esta configuracion afecta los tiempos de ejecución y el comportamiento
@@ -9,28 +10,50 @@ export class Person {
     //     private address: string = 'No address',
     // ) {}
 
-    constructor(name: string, address: string = 'No Address') {
-        this.name = name;
+    constructor(name: string, lastName: string, address: string = 'No Address') {
+        this.firstName = name;
+        this.lastName = lastName;
         this.address = address;
     }
 }
 
-export class Hero extends Person {
+// export class Hero extends Person {
+//     public alterEgo: string;
+//     public age: number;
+//     public realName: string;
+
+//     constructor(
+//          alterEgo: string,
+//          age: number,
+//          realName: string,
+//     ) {
+//         super(realName, 'New York');
+//         this.alterEgo = alterEgo;
+//         this.age = age;
+//         this.realName = realName;
+//     }
+// }
+
+// Example of dependency injection
+export class Hero {
     public alterEgo: string;
     public age: number;
     public realName: string;
+    public person: Person;
 
     constructor(
-         alterEgo: string,
-         age: number,
-         realName: string,
+        alterEgo: string,
+        age: number,
+        realName: string,
+        person: Person,
     ) {
-        super(realName, 'New York');
         this.alterEgo = alterEgo;
         this.age = age;
         this.realName = realName;
+        this.person = person;
     }
 }
-const ironman = new Hero('Ironman', 45, 'Tony Stark');
+const tony = new Person('Tony', 'Stark', 'New York');
+const ironman = new Hero('Ironman', 45, 'Tony Stark', tony);
 
 console.log(ironman);
