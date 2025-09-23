c633ab9bf3a104cdfc68dec1485b8fea613b29c6
Josué David Hernández Ramírez <josue.hernandezr@motivus.com>
2025-09-23 13:31:59 -0600

    feat: optional chaining 🎨

--- typescript-intro/src/main.ts
+++ typescript-intro/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 // import './topics/basic-types';
 // import './topics/object-interfaces';
-import './topics/classes';
+// import './topics/classes';
+import './topics/optional-chaining';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
