aea470b5ec771f2fa8b40530c9a9c171cf00a0fa
Josué David Hernández Ramírez <josue.hernandezr@motivus.com>
2025-10-02 20:10:24 -0600

    feat: lifecycle hooks 📚

---
+++ lifecycle-hooks/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: '**', redirectTo: '' },
+];
