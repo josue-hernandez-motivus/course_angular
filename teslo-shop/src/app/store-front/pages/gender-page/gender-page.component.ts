2e0eb6fea84cfd813dd5f4a9d7c65a95bb0b439c
Josué David Hernández Ramírez <josue.hernandezr@motivus.com>
2025-10-03 00:47:31 -0600

    feat: implement swiper, and show all products 🪲

--- teslo-shop/src/app/store-front/pages/gender-page/gender-page.component.ts
+++ teslo-shop/src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { ProductCardComponent } from '@/products/components/product-card/product-card.component';
+import { ProductsService } from '@/products/services/products.service';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
-  selector: 'app-gender-page',
-  imports: [],
+  selector: 'gender-page',
+  imports: [ProductCardComponent],
   templateUrl: './gender-page.component.html',
 })
-export class GenderPageComponent { }
+export class GenderPageComponent {
+  route = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+  productsResource = rxResource({
+    params: () => ({ gender: this.gender() }),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({
+        gender: params.gender,
+      });
+    },
+  });
+}
