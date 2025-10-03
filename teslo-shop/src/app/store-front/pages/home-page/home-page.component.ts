807ba2b44f494971fe411465b855c9bcdf5e13ee
Josué David Hernández Ramírez <josue.hernandezr@motivus.com>
2025-10-03 01:29:49 -0600

    feat: add pagination and cache 📚

--- teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,23 +1,41 @@
 import { ProductCardComponent } from '@/products/components/product-card/product-card.component';
 import { ProductsService } from '@/products/services/products.service';
+import { PaginationComponent } from '@/shared/components/pagination/pagination.component';
+import { PaginationService } from '@/shared/services/pagination.service';
 import { Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
   productsResource = rxResource({
-    params: () => ({}),
+    params: () => ({ page: this.paginationService.currentPage() - 1 }),
     stream: ({ params }) => {
-      return this.productsService.getProducts({});
+      return this.productsService.getProducts({
+        offset: params.page * 9,
+      });
     },
   });
+
+
+  // activatedRoute = inject(ActivatedRoute);
+
+  // currentPage = toSignal(
+  //   this.activatedRoute.queryParamMap.pipe(
+  //     map((params) => (params.get('page') ? +params.get('page')! : 1)),
+  //     map((page) => (isNaN(page) ? 1 : page))
+  //   ),
+  //   {
+  //     initialValue: 1,
+  //   }
+  // );
  }
 
 
